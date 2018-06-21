8691240a902ca9b0a760ea8b1001ae9daac24414
LAgagggggg <15807814960@163.com>
2018-06-21 23:23:15 +0800

    Rectangle Area

---
+++ Rectangle Area.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func computeArea(_ A: Int, _ B: Int, _ C: Int, _ D: Int, _ E: Int, _ F: Int, _ G: Int, _ H: Int) -> Int {
+        if E>=A || G<=A || H<=B || F>=D {
+        	return ((C-A)*(D-B)+(G-E)*(H-F))
+        }
+        else {
+        	let Warr=[A,C,E,G].sorted()
+        	let Harr=[B,D,F,H].sorted()
+        	let Width=abs(Warr[1]-Warr[2])
+        	let Height=abs(Harr[1]-Harr[2])
+        	return	((C-A)*(D-B)+(G-E)*(H-F))-Width*Height
+        }
+    }
+}
