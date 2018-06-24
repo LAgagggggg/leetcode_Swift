d1f4713faa9c9af21b23a9312b2034936f14c468
LAgagggggg <15807814960@163.com>
2018-06-24 20:56:36 +0800

    ZIP

--- ZigZag Conversion.swift
+++ ZigZag Conversion.swift
@@ -1,32 +1,43 @@
 class Solution {
     func convert(_ s: String, _ numRows: Int) -> String {
-    	let letterPerCycle=(2*numRows-2)
+        let letterPerCycle=(2*numRows-2)
+        if letterPerCycle<=0 || s.count<=numRows {
+            return s
+        }
         let colCnt=s.count/letterPerCycle
         let letterInMid=letterPerCycle - numRows
-        let remainedCnt=s.count-colCnt*letterPerCycle
-        var resultS:String
+        let remainedCnt=(colCnt+1)*letterPerCycle-s.count
+        var resultS=""
         var modifiedS=s
         for _ in 0..<remainedCnt{
-        	modifiedS+="*"
+            modifiedS+="*"
         }
         var i=0
         while i<modifiedS.count{
-        	resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
-        	i+=letterPerCycle
+            resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
+            i+=letterPerCycle
         }
-        for j in 1...letterInMid{
-        	i=j
-        	while i<modifiedS.count{
-        		resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
-        		resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i+letterPerCycle-2*j)])
-        		i+=letterPerCycle
-        	}
+        if letterInMid>=1 {
+            for j in 1...letterInMid{
+                i=j
+                while i<modifiedS.count{
+                    resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
+                    resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i+letterPerCycle-2*j)])
+                    i+=letterPerCycle
+                }
+            }
         }
-        i=numRows
+        i=numRows-1
         while i<modifiedS.count{
-        	resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
-        	i+=letterPerCycle
+            resultS+=String(modifiedS[modifiedS.index(modifiedS.startIndex, offsetBy:i)])
+            i+=letterPerCycle
         }
-        return resultS.replacingOccurrences(of:"*", with: "")
+        while let index=resultS.index(of: "*") {
+            resultS.remove(at: index)
+        }
+        return resultS
     }
 }
+let sol = Solution()
+print(sol.convert("PAYPALISHIRING", 3))
+
