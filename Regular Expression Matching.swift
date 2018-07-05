e2d1e71f9ed26ecf91736aa1efc54943d78e2af1
LAgagggggg <15807814960@163.com>
2018-07-05 10:09:02 +0800

    so fucking wrong

--- Regular Expression Matching.swift
+++ Regular Expression Matching.swift
@@ -6,12 +6,16 @@ class Solution {
         let pCnt=p.count
         var i=0,j=0
         mainWhile: while i<sCnt && j<pCnt {
+            if i<0 {
+                i=0
+            }
             var isNextStar=false
             let theCharacter=pArr[j]
             if j != pCnt-1 { isNextStar = pArr[j+1]=="*" ? true:false}
             if isNextStar {// is "*"?
                 let nextRequire:Character? = j+2<pCnt ? pArr[j+2]:nil
                 var backCnt=0
+                var nextRequireArr:[Character]=Array()
                 if theCharacter==nextRequire && theCharacter != "."{//
                     while(j+2+backCnt<pCnt && pArr[j+2+backCnt]==theCharacter){
                         backCnt+=1
@@ -20,7 +24,11 @@ class Solution {
                 else if(j+3<pCnt && pArr[j+3]=="*"){
                     var ignoreCnt=3
                     while(j+ignoreCnt<pCnt && pArr[j+ignoreCnt]=="*"){
+                        nextRequireArr.append(pArr[j+ignoreCnt-1])
                         ignoreCnt+=2
+                    }
+                    if(j+ignoreCnt-1<pCnt){
+                        nextRequireArr.append(pArr[j+ignoreCnt-1])
                     }
                     ignoreCnt-=1
                     while(j+ignoreCnt+backCnt<pCnt && pArr[j+ignoreCnt+backCnt]==theCharacter){
@@ -41,7 +49,26 @@ class Solution {
                             }
                         }
                     }
-                    i-=backCnt
+                    for k in 0..<nextRequireArr.count{
+                        let c=nextRequireArr[k]
+                        if c==sArr[i] && c != theCharacter{
+                            j+=2
+                            continue mainWhile
+                        }
+                        else if c==theCharacter && k==nextRequireArr.count-1{
+                            i-=backCnt
+                            j+=2
+                            continue mainWhile
+                        }
+                        else if c=="." && k==nextRequireArr.count-1{
+                            print("fuck")
+                            j+=2
+                            continue mainWhile
+                        }
+                        else{
+                            j+=2
+                        }
+                    }
                     if sArr[i] != nextRequire && nextRequire != "."{
                         return false
                     }
@@ -56,6 +83,7 @@ class Solution {
                             i+=1
                             if !(i<sCnt) {break mainWhile}
                         }
+                        j+=2
                         continue mainWhile
                     }
                     else{
@@ -95,4 +123,4 @@ class Solution {
 }
 
 let s=Solution()
-print(s.isMatch("aaca", "ab*a*c*a"))
+print(s.isMatch("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*"))
